1c40a63e0eee297c5a6b5e9b72953c955f1894d2
meiteng05 <62166410+meiteng05@users.noreply.github.com>
2022-03-24 17:25:04 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cdxc9ex8iiqz.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "tehmeiteng-employee"
+customregion = "us-east-1"
